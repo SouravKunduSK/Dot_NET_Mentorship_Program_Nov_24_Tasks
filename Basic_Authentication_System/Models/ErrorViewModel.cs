021583b5b7bfe3a26c53f92cec50e22ca3cb16d9
Sourav Kundu <74766939+SouravKunduSK@users.noreply.github.com>
2024-11-19 23:38:40 +0600

    Created new project

---
+++ Basic_Authentication_System/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Basic_Authentication_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
