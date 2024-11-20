2a823ccf186c2876d85515d73ce5bb535e88770f
Sourav Kundu <74766939+SouravKunduSK@users.noreply.github.com>
2024-11-20 23:28:34 +0600

    Added Password helper for hashing password

---
+++ Basic_Authentication_System/Helpers/PasswordHelper.cs
@@ -0,0 +1,27 @@
+﻿using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Basic_Authentication_System.Helpers
+{
+    public static class PasswordHelper
+    {
+        //Hashing Password
+        public static string HashPassword(string password)
+        {
+            using var sha256 = SHA256.Create();
+            var bytes = Encoding.UTF8.GetBytes(password);
+            var hash = sha256.ComputeHash(bytes);
+            password = Convert.ToBase64String(hash);
+            return password;
+        }
+
+        //Verifying password
+        public static bool VerifyPassword(string realPassword, string password)
+        {
+            var result = (realPassword == HashPassword(password)) ? false : true;
+            return result;
+        }
+
+    }
+}
